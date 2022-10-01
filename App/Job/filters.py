f9c18b4ed32e6acac477f2dae83af745d59a08bd
Nirajan Sangraula <neerazansangraula14@gmail.com>
2022-09-30 23:51:57 -0500

    some changes

--- App/Job/filters.py
+++ App/Job/filters.py
@@ -6,5 +6,5 @@ from .models import *
 class JobsFilter(django_filters.FilterSet):
     note = CharFilter(field_name='name',lookup_expr='icontains')
     class Meta:
-        model = Jobs
+        model = Job
         fields= ['city','state','country','zip_code']
